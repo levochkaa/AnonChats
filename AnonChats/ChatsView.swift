5aa92f77e9c5763c29b23c27d7e1aeb68472a6a3
Lev Poznyakov <levfirstgamer@mail.ru>
2022-02-08 12:17:11 +0300

    List of Chats

--- AnonChats/ChatsView.swift
+++ AnonChats/ChatsView.swift
@@ -2,7 +2,24 @@
 import SwiftUI
 
 struct ChatsView: View {
+
+    @EnvironmentObject var viewModel: Chats
+
     var body: some View {
-        Text("Chats")
+        List(viewModel.chats) { chat in
+            NavigationLink(destination: ChatView().environmentObject(viewModel)) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(chat.title)
+                            .font(.system(size: 18, weight: .semibold, design: .rounded))
+                        Text(chat.topic)
+                            .font(.system(size: 14, weight: .regular, design: .rounded))
+                    }
+                    Spacer()
+                    Text("\(chat.users.count)/\(chat.maxUsers)")
+                        .font(.system(size: 14, weight: .semibold, design: .rounded))
+                } .padding(.vertical, 10)
+            }
+        } .listStyle(.plain)
     }
 }
