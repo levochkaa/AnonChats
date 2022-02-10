b2e8c696e6cc607843371f41b5caa64d04fddc98
Lev Poznyakov <levfirstgamer@mail.ru>
2022-02-10 16:33:47 +0300

    Sort MenuView

---
+++ AnonChats/MenuView.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+
+struct MenuView: View {
+
+    @EnvironmentObject var viewModel: Chats
+
+    var body: some View {
+        Menu {
+            Button(action: {
+                self.viewModel.query = 1
+            }) {
+                HStack {
+                    Text("Default")
+                    Spacer()
+                    Image(systemName: "person.fill")
+                }
+            }
+            Button(action: {
+                self.viewModel.query = 2
+            }) {
+                HStack {
+                    Text("By 2 people")
+                    Spacer()
+                    Image(systemName: "person.2.fill")
+                }
+            }
+            Button(action: {
+                self.viewModel.query = 3
+            }) {
+                HStack {
+                    Text("By 3 people")
+                    Spacer()
+                    Image(systemName: "person.3.sequence.fill")
+                }
+            }
+        } label: {
+            Button(action: {
+                //
+            }) {
+                Text("Sort")
+            }
+        }
+    }
+}
