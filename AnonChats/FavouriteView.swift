4cdfcabbe72899fc26d0878a99df5d8877c8bbda
Lev Poznyakov <levfirstgamer@mail.ru>
2022-02-14 01:20:47 +0300

    FavouriteView

--- AnonChats/FavouriteView.swift
+++ AnonChats/FavouriteView.swift
@@ -1,8 +1,40 @@
-
 import SwiftUI
 
 struct FavouriteView: View {
+
+    @State var isActive: Bool = false
+    @State private var query = ""
+    @EnvironmentObject var viewModel: Chats
+
     var body: some View {
-        Text("Favourite")
+        List(viewModel.getFavouriteFilteredChats(query: query)) { chat in
+            NavigationLink(destination: ChatView(chat: chat, chats: viewModel, rootIsActive: self.$isActive), isActive: self.$isActive) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text(chat.title)
+                            .font(.system(size: 18, weight: .semibold, design: .rounded))
+                        Text(chat.topic)
+                            .font(.system(size: 14, weight: .regular, design: .rounded))
+                    }
+                    Spacer()
+                    Text("\(chat.users.count)/\(chat.maxUsers)")
+                        .font(.system(size: 14, weight: .semibold, design: .rounded))
+                } .padding(.vertical, 10)
+            }
+            .isDetailLink(false)
+            .contextMenu {
+                Button(action: {
+                    viewModel.removeFromFavourite(id: chat.id)
+                }) {
+                    Image(systemName: "star")
+                    Text("Remove from favourite")
+                }
+            }
+        }
+        .listStyle(.plain)
+        .searchable(text: $query)
+        .refreshable {
+            viewModel.fetchData()
+        }
     }
 }
