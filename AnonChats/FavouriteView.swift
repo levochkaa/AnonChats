7a5b2d177cc44e9181fcc616131ec824629cd8e8
Lev Poznyakov <levfirstgamer@mail.ru>
2022-02-18 13:06:05 +0300

    line limit fix

--- AnonChats/FavouriteView.swift
+++ AnonChats/FavouriteView.swift
@@ -14,14 +14,17 @@ struct FavouriteView: View {
                     VStack(alignment: .leading) {
                         Text(chat.title)
                             .font(.system(size: 18, weight: .semibold, design: .rounded))
+                            .lineLimit(1)
                         Text(chat.topic)
                             .font(.system(size: 14, weight: .regular, design: .rounded))
+                            .lineLimit(1)
                     }
                     Spacer()
                     Text("\(chat.users.count)/\(chat.maxUsers)")
                         .font(.system(size: 14, weight: .semibold, design: .rounded))
                 } .padding(.vertical, 10)
             }
+            .disabled(chat.maxUsers == chat.users.count && !chat.users.contains(viewModel.user!.uid))
             .contextMenu {
                 Button(action: {
                     viewModel.removeFromFavourite(id: chat.id)
